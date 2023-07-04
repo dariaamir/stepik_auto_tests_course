bbe82fe86ac1a78840d40a9d1b7217cc25fd5d2a
dariaamir <19853206+dariaamir@users.noreply.github.com>
2023-07-04 14:39:17 +0300

    Added task for Lesson 3.3 and optimised it so test doesn't fail

---
+++ lesson3_3_step3.py
@@ -0,0 +1,46 @@
+import pytest
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+import time
+
+
+class TestRegistration():
+    browser = webdriver.Chrome()
+    url1 = 'http://suninjuly.github.io/registration1.html'
+    url2 = 'http://suninjuly.github.io/registration2.html'
+
+    welcome_text = "Congratulations! You have successfully registered!"
+
+    def test_registration_1_happy_path(self):
+        self.browser.get(url=self.url1)
+
+        input_first_name = self.browser.find_element(By.CSS_SELECTOR, ".first_block .first_class input")
+        input_last_name = self.browser.find_element(By.CSS_SELECTOR, ".first_block .second_class input")
+        input_email = self.browser.find_element(By.CSS_SELECTOR, ".first_block .third_class input")
+
+
+        input_first_name.send_keys("Ivan")
+        input_last_name.send_keys("Petrov")
+        input_email.send_keys("ivan_petrov@mail.ru")
+
+        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        time.sleep(1)
+        welcome_text_elt = self.browser.find_element(By.TAG_NAME, "h1")
+
+        assert welcome_text_elt.text == self.welcome_text, \
+        f"Error: registration text is not correct. " \
+        f"Actual: {welcome_text_elt.text}, expected: {self.welcome_text}"
+
+    def test_registration_2_negative_flow(self):
+        self.browser.get(url=self.url2)
+        input_first_name = self.browser.find_element(By.CSS_SELECTOR, ".first_block .first_class input")
+        try:
+            with pytest.raises(NoSuchElementException):
+                self.browser.find_element(By.CSS_SELECTOR, ".first_block .second_class input")
+                pytest.fail("Не должно быть второго поля вводу")
+        finally:
+            self.browser.quit()
